853c57d8c15c825307fa3b27958b601215404a51
ProvaVesco <full_@hotmail.it>
2016-11-07 21:59:44 +0100

    Omar Branch

--- MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/WiFiDirectBroadcastReceiver.java
+++ MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/WiFiDirectBroadcastReceiver.java
@@ -14,22 +14,17 @@ import java.nio.channels.Channel;
 
 public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
 
-       
     private WifiP2pManager mManager;
-       
     private Channel mChannel;
-       
     private MetronomeActivity mActivity;
 
-       
 
-    public WiFiDirectBroadcastReceiver(WifiP2pManager manager, Channel channel,
-                                                  MetronomeActivity activity) {
-            super();
-            this.mManager = manager;
-            this.mChannel = channel;
-            this.mActivity = activity;
-          }
+    public WiFiDirectBroadcastReceiver(WifiP2pManager manager, Channel channel, MetronomeActivity activity) {
+        super();
+        this.mManager = manager;
+        this.mChannel = channel;
+        this.mActivity = activity;
+    }
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -37,12 +32,12 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
 
         if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
             // Check to see if Wi-Fi is enabled and notify appropriate activity
-                } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
-                   // Call WifiP2pManager.requestPeers() to get a list of current peers
-                } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
-                   // Respond to new connection or disconnections
-                } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
-                   // Respond to this device's wifi state changing
-                }
-          }
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+            // Call WifiP2pManager.requestPeers() to get a list of current peers
+        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+            // Respond to new connection or disconnections
+        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+            // Respond to this device's wifi state changing
+        }
+    }
 }
