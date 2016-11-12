ae9d1e14b76f0091c5079f268459deb27452328c
ProvaVesco <full_@hotmail.it>
2016-11-12 18:27:17 +0100

    Omar_Service

--- MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/sound/management/SoundManagerServiceCaller.java
+++ MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/sound/management/SoundManagerServiceCaller.java
@@ -1,6 +1,10 @@
 package a2016.soft.ing.unipd.metronomepro.sound.management;
 
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 
 /**
  * Created by feder on 12/11/2016.
@@ -10,27 +14,51 @@ import android.content.Context;
 
 public class SoundManagerServiceCaller implements SoundManager {
 
-    public SoundManagerServiceCaller(Context c){
+    private SoundManagerService mService;
+    private Context cService;
+    private boolean mBound;     //Flag che indica se è stato chiamato il bind sul servizio
 
+    private ServiceConnection mConnection = new ServiceConnection() {
 
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service) {
+
+            // We've bound to SoundManagerService, cast the IBinder and get LocalService instance
+            SoundManagerService.myBinder binder = (SoundManagerService.myBinder) service;
+            mService = binder.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mBound = false;
+        }
+    };
+
+    public SoundManagerServiceCaller(Context c) {
+        cService = c;
     }
 
     @Override
     public void initialize(int maxBPM, int minBPM) {
 
-        //Creo il Bundle, aggiunti i parametri e avvio il servizio con il Bundle
+        //Creo il Bundle (Intent), aggiunti i parametri e avvio il servizio con il Bundle
         //Le stringhe vanno memorizzate nello stesso posto
 
+        Intent intent = new Intent(cService, SoundManagerService.class);
+        cService.bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
     }
 
     @Override
     public void play() {
 
+        mService.play();
     }
 
     @Override
     public void stop() {
 
+        mService.stop();
     }
 
     @Override
@@ -46,5 +74,6 @@ public class SoundManagerServiceCaller implements SoundManager {
     @Override
     public void setBPM(int newBPM) {
 
+        mService.setBPM(newBPM);
     }
 }
