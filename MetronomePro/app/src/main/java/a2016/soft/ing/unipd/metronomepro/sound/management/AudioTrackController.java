40458a048ea4fded762b0f9aa220b423bc88322f
GiulioPettenuzzo <giulio.pettenuzzo@studenti.unipd.it>
2016-11-12 15:30:10 +0100

    metodo LoadFile copleatato, legge i due file e li inserisce in un array di byte

--- MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/sound/management/AudioTrackController.java
+++ MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/sound/management/AudioTrackController.java
@@ -1,8 +1,8 @@
 package a2016.soft.ing.unipd.metronomepro.sound.management;
 
 import java.io.FileDescriptor;
-
-import a2016.soft.ing.unipd.metronomepro.*;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 /**
  * Created by feder on 12/11/2016.
@@ -13,13 +13,22 @@ import a2016.soft.ing.unipd.metronomepro.*;
 
 public class AudioTrackController implements SoundManager {
 
+    private byte[] initialClack; //metto i due array come variabili per potervi accedere da fuori
+    private byte[] finalClack;
     /**
      * Questo metodo carica questi due file in due array, per poi poterli gestire
      * @param clack il clack normale
      * @param clackFinal il clack di fine battuta
      */
-    void loadFile(FileDescriptor clack, FileDescriptor clackFinal){
-
+    void loadFile(FileDescriptor clack, FileDescriptor clackFinal) {
+        //inizializzo i due array di byte
+        try {
+            FileInputStream streamInClack = new FileInputStream(clack);
+            FileInputStream streamFinClack = new FileInputStream(clackFinal);
+            initialClack = new byte[streamInClack.available()];
+            finalClack = new byte[streamFinClack.available()];
+        } catch (IOException e) {
+        }
     }
 
     @Override
@@ -29,7 +38,7 @@ public class AudioTrackController implements SoundManager {
 
     @Override
     public void play() {
-
+//audio track = audiotrack.play();
     }
 
     @Override
