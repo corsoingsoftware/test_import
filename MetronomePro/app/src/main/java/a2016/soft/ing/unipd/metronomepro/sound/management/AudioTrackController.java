ec0790228f6254cd8faaacec81a845e8a1026eb1
GiulioPettenuzzo <giulio.pettenuzzo@studenti.unipd.it>
2016-11-12 21:10:44 +0100

    ho creato l'array di silenzio ma devo accodarlo agli array di byte

--- MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/sound/management/AudioTrackController.java
+++ MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/sound/management/AudioTrackController.java
@@ -4,6 +4,7 @@ import java.io.FileDescriptor;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 /**
  * Created by feder on 12/11/2016.
@@ -14,24 +15,30 @@ import java.io.IOException;
 
 public class AudioTrackController implements SoundManager {
 
+    private static final int WAV_HEADER_SIZE_IN_BYTES = 44;
     private byte[] initialClack; //metto i due array come variabili per potervi accedere da fuori
     private byte[] finalClack;
+    private ByteBuffer silence = ByteBuffer.allocate(2);
+
     /**
      * Questo metodo carica questi due file in due array, per poi poterli gestire
      * @param clack il clack normale
      * @param clackFinal il clack di fine battuta
      */
     void loadFile(FileDescriptor clack, FileDescriptor clackFinal) {
-        //inizializzo i due array di byte
         try {
+            //inizializzo i due array di byte
             FileInputStream streamInClack = new FileInputStream(clack);
             FileInputStream streamFinClack = new FileInputStream(clackFinal);
             byte[] initialClack1 = new byte[streamInClack.available()];
             byte[] finalClack1 = new byte[streamFinClack.available()];
 
+            //inizializzo il byte di silenzio
+            silence.putShort(Short.MIN_VALUE);
+            //TODO: accodare questo buffer ad entrambi gli arrey di byte
             //adesso tolgo gli header di 44 byte all'inizio dei file
-            initialClack = new byte[initialClack1.length - 44];
-            finalClack = new byte[finalClack1.length - 44];
+            initialClack = new byte[initialClack1.length - WAV_HEADER_SIZE_IN_BYTES];
+            finalClack = new byte[finalClack1.length - WAV_HEADER_SIZE_IN_BYTES];
             //legge i file e li salva nei due array
             try {
                 streamInClack.read(initialClack1);
@@ -48,12 +55,12 @@ public class AudioTrackController implements SoundManager {
 
             //mi trovo con due array di dimensioni clack -44Byte
             //adesso li riempio
-            for (int i = 43; i < initialClack1.length; i++) {
+            for (int i = WAV_HEADER_SIZE_IN_BYTES - 1; i < initialClack1.length; i++) {
                 int j = 0;
                 initialClack[j] = initialClack1[i];
                 j = j + 1;
             }
-            for (int i = 43; i < finalClack1.length; i++) {
+            for (int i = WAV_HEADER_SIZE_IN_BYTES - 1; i < finalClack1.length; i++) {
                 int j = 0;
                 finalClack[j] = finalClack1[i];
                 j = j + 1;
