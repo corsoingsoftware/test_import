f457bf528a56fd54f01bdf4a5070b70157f37fa8
GiulioPettenuzzo <giulio.pettenuzzo@studenti.unipd.it>
2016-11-14 13:57:21 +0100

    accodato il silenzio, fatta una bozza di play() e stop()

--- MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/sound/management/AudioTrackController.java
+++ MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/sound/management/AudioTrackController.java
@@ -1,5 +1,9 @@
 package a2016.soft.ing.unipd.metronomepro.sound.management;
 
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+
 import java.io.FileDescriptor;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -16,10 +20,11 @@ import java.nio.ByteBuffer;
 public class AudioTrackController implements SoundManager {
 
     private static final int WAV_HEADER_SIZE_IN_BYTES = 44;
+    private static final int SAMPLE_RATE_IN_HERTZ = 44100;
     private byte[] initialClack; //metto i due array come variabili per potervi accedere da fuori
     private byte[] finalClack;
     private ByteBuffer silence = ByteBuffer.allocate(2);
-
+    private AudioTrack at;
     /**
      * Questo metodo carica questi due file in due array, per poi poterli gestire
      * @param clack il clack normale
@@ -30,16 +35,17 @@ public class AudioTrackController implements SoundManager {
             //inizializzo i due array di byte
             FileInputStream streamInClack = new FileInputStream(clack);
             FileInputStream streamFinClack = new FileInputStream(clackFinal);
+            //array temporanei
             byte[] initialClack1 = new byte[streamInClack.available()];
             byte[] finalClack1 = new byte[streamFinClack.available()];
 
             //inizializzo il byte di silenzio
             silence.putShort(Short.MIN_VALUE);
-            //TODO: accodare questo buffer ad entrambi gli arrey di byte
             //adesso tolgo gli header di 44 byte all'inizio dei file
-            initialClack = new byte[initialClack1.length - WAV_HEADER_SIZE_IN_BYTES];
-            finalClack = new byte[finalClack1.length - WAV_HEADER_SIZE_IN_BYTES];
-            //legge i file e li salva nei due array
+            //inizializzo i due array al doppio della dimensione del suono
+            initialClack = new byte[initialClack1.length * 2 - WAV_HEADER_SIZE_IN_BYTES];
+            finalClack = new byte[finalClack1.length * 2 - WAV_HEADER_SIZE_IN_BYTES];
+            //legge i file e li salva nei due array temoranei
             try {
                 streamInClack.read(initialClack1);
                 streamFinClack.close();
@@ -53,8 +59,8 @@ public class AudioTrackController implements SoundManager {
                 e.printStackTrace();
             }
 
-            //mi trovo con due array di dimensioni clack -44Byte
-            //adesso li riempio
+            //mi trovo con due array provvisori riepiti col suono
+            //adesso tolgo gli header e salvo il suono nei due array difinitivi
             for (int i = WAV_HEADER_SIZE_IN_BYTES - 1; i < initialClack1.length; i++) {
                 int j = 0;
                 initialClack[j] = initialClack1[i];
@@ -65,6 +71,14 @@ public class AudioTrackController implements SoundManager {
                 finalClack[j] = finalClack1[i];
                 j = j + 1;
             }
+            //accodo il silenzio nei due array
+            for (int i = finalClack1.length - WAV_HEADER_SIZE_IN_BYTES; i < finalClack.length; i++) {
+                finalClack[i] = silence.get();
+            }
+            for (int i = initialClack1.length - WAV_HEADER_SIZE_IN_BYTES; i < initialClack.length; i++) {
+                initialClack[i] = silence.get();
+            }
+            //a questo punto mi trovo con due array con i rispettivi suoni senza gli header con accodati i silenzi!
         } catch (IOException e) {
         }
     }
@@ -77,11 +91,21 @@ public class AudioTrackController implements SoundManager {
     @Override
     public void play() {
 //audio track = audiotrack.play();
+        //inizializzo AudioTrack, COPIATO SPUTATO DA FEDERICO: SOUNDMANAGER
+        int channelConfig = AudioFormat.CHANNEL_OUT_STEREO;
+        int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+        int intSize = android.media.AudioTrack.getMinBufferSize(SAMPLE_RATE_IN_HERTZ, channelConfig, audioFormat);
+        at = new AudioTrack(AudioManager.STREAM_MUSIC, SAMPLE_RATE_IN_HERTZ, channelConfig,
+                audioFormat, intSize, AudioTrack.MODE_STATIC);
+        //scrivo un array di byte in AudioTrack
+        at.write(initialClack, 0, initialClack.length);
+        at.play();
+        //TODO: gli array sono due! bisogna fare due play??
     }
 
     @Override
     public void stop() {
-
+        at.stop();
     }
 
     @Override
