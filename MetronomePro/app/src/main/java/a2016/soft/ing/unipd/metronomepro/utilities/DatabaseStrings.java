e4e3214bc06c35cb46d6cf3c02dbd02fbdfcc516
Francesco Loro <francesco.official@gmail.com>
2016-12-09 11:37:31 +0100

    tolto un brutto hard coding

---
+++ MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/utilities/DatabaseStrings.java
@@ -0,0 +1,18 @@
+package a2016.soft.ing.unipd.metronomepro.utilities;
+
+/**
+ * Created by Francesco on 08/12/2016.
+ * Questa classe contiene i nomi e gli id degli attributi alle tabelle del database
+ */
+
+public class DatabaseStrings {
+
+    public static final String DBNAME = "MetronomeProDatabase";
+    public static final int DB_VERSION = 1;
+    public static final String TBL_TRACCE = "Tracce";
+    public static final long FIELD_T_ID = 0;
+    public static final String FIELD_T_NAME = "Nome";
+    public static final String TBL_PLAYLIST = "Playlist";
+    public static final long FIELD_P_ID = 0;
+    public static final String FIELD_P_NAME = "Nome";
+}
