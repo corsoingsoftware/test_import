a6dcac2787d76c9c583c1976ee6e5498c3e23e32
FedericoFavotto <federico.favotto94@gmail.com>
2016-12-09 11:17:36 +0100

    implemented silence method

--- MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/sound/management/generators/SignalsGenerator.java
+++ MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/sound/management/generators/SignalsGenerator.java
@@ -2,6 +2,9 @@ package a2016.soft.ing.unipd.metronomepro.sound.management.generators;
 
 import android.media.AudioFormat;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
 /**
  * Created by feder on 08/12/2016.
  */
@@ -11,6 +14,10 @@ public class SignalsGenerator {
      * Audio frequency
      */
     private static final int DEFAULT_SAMPLE_RATE_IN_HERTZ = 8000;
+    /**
+     * Size of short data
+     */
+    private static final int SHORT_SIZE=2;
 
     /**
      * 2 bytes per sample default (short unsigned)
@@ -50,7 +57,7 @@ public class SignalsGenerator {
      * @param lengthInBytes lenght in bytes of sinus
      * @return sinus sound in bytes to play it with current configs
      */
-    public byte[] generateSin(long lengthInBytes, int sinFrequency){
+    public byte[] generateSin(int lengthInBytes, int sinFrequency){
         return null;
     }
 
@@ -59,7 +66,18 @@ public class SignalsGenerator {
      * @param lengthInByte
      * @return array of bytes to play
      */
-    public byte[] silence(long lengthInByte){
-        return null;
+    public byte[] silence(int lengthInByte){
+        byte[] toReturn= new byte[lengthInByte];
+        ByteBuffer bb = ByteBuffer.allocate(SHORT_SIZE);
+        bb.order(ByteOrder.LITTLE_ENDIAN);
+        bb.putShort(Short.MIN_VALUE);
+        bb.position(0);
+        byte first=bb.get();
+        byte second=bb.get();
+        for(int i=0;i<toReturn.length-1;i++){
+            toReturn[i++]=first;
+            toReturn[i]=second;
+        }
+        return toReturn;
     }
 }
