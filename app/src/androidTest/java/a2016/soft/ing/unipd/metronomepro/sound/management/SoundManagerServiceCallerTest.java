0d75b729c2ab23c4bf36d13f6db6cc20a2c139d5
FedericoFavotto <federico.favotto94@gmail.com>
2016-12-12 08:44:26 +0100

    Prova fix problema
    Spostata directory

---
+++ app/src/androidTest/java/a2016/soft/ing/unipd/metronomepro/sound/management/SoundManagerServiceCallerTest.java
@@ -0,0 +1,46 @@
+package a2016.soft.ing.unipd.metronomepro.sound.management;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by feder on 30/11/2016.
+ */
+public class SoundManagerServiceCallerTest {
+    @Test
+    public void initialize() throws Exception {
+
+    }
+
+    @Test
+    public void getBPM() throws Exception {
+
+    }
+
+    @Test
+    public void play() throws Exception {
+
+    }
+
+    @Test
+    public void stop() throws Exception {
+
+    }
+
+    @Test
+    public void getState() throws Exception {
+
+    }
+
+    @Test
+    public void setRhythmics() throws Exception {
+
+    }
+
+    @Test
+    public void setBPM() throws Exception {
+
+    }
+
+}
