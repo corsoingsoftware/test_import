d91d38a67001e8bd4c521c249bc5c6ffbadb3e3b
FedericoFavotto <federico.favotto94@gmail.com>
2016-12-12 14:17:49 +0100

    Commit per Francesco

--- MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/SongCreator.java
+++ MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/SongCreator.java
@@ -4,10 +4,22 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
 
-public class SongCreator extends AppCompatActivity {
+import a2016.soft.ing.unipd.metronomepro.adapters.TimeSlicesAdapter;
+import a2016.soft.ing.unipd.metronomepro.adapters.touch.helpers.DragTouchHelperCallback;
+import a2016.soft.ing.unipd.metronomepro.adapters.touch.helpers.OnStartDragListener;
+
+public class SongCreator extends AppCompatActivity implements OnStartDragListener {
+
+    private RecyclerView rVTimeSlices;
+    private RecyclerView.LayoutManager rVLayoutManager;
+    private TimeSlicesAdapter timeSlicesAdapter;
+    private ItemTouchHelper itemTouchHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +27,15 @@ public class SongCreator extends AppCompatActivity {
         setContentView(R.layout.activity_song_creator);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        rVTimeSlices = (RecyclerView)  findViewById(R.id.recycler_view_time_slices);
+        rVTimeSlices.setHasFixedSize(true);
+        rVLayoutManager =  new LinearLayoutManager(this);
+        rVTimeSlices.setLayoutManager(rVLayoutManager);
+        timeSlicesAdapter = new TimeSlicesAdapter(this, this);
+        rVTimeSlices.setAdapter(timeSlicesAdapter);
+        DragTouchHelperCallback myItemTouchHelper = new DragTouchHelperCallback(timeSlicesAdapter);
+        itemTouchHelper = new ItemTouchHelper(myItemTouchHelper);
+        itemTouchHelper.attachToRecyclerView(rVTimeSlices);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,4 +46,8 @@ public class SongCreator extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onStartDrag(RecyclerView.ViewHolder viewHolder) {
+        itemTouchHelper.startDrag(viewHolder);
+    }
 }
--- MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/adapters/TimeSlicesAdapter.java
+++ MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/adapters/TimeSlicesAdapter.java
@@ -1,13 +1,19 @@
 package a2016.soft.ing.unipd.metronomepro.adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Collection;
+import java.util.Collections;
+
 import a2016.soft.ing.unipd.metronomepro.adapters.touch.helpers.ItemTouchHelperAdapter;
 import a2016.soft.ing.unipd.metronomepro.adapters.touch.helpers.ItemTouchHelperViewHolder;
+import a2016.soft.ing.unipd.metronomepro.adapters.touch.helpers.OnStartDragListener;
+import a2016.soft.ing.unipd.metronomepro.entities.Song;
 
 /**
  * Created by feder on 12/12/2016.
@@ -15,15 +21,43 @@ import a2016.soft.ing.unipd.metronomepro.adapters.touch.helpers.ItemTouchHelperV
 
 public class TimeSlicesAdapter extends RecyclerView.Adapter<TimeSlicesAdapter.ViewHolder> implements ItemTouchHelperAdapter {
 
+    private Context context;
+    /**
+     * It must be final cause can't change
+     */
+    private final OnStartDragListener dragListener;
+    private Song songToEdit;
+
+
+    public TimeSlicesAdapter(Context context, OnStartDragListener dragListener){
+        this(context,dragListener,null);
+    }
+
+    public TimeSlicesAdapter(Context context, OnStartDragListener dragListener, Song song) {
+        this.dragListener=dragListener;
+        this.context=context;
+        this.songToEdit=song;
+    }
+
+    @Override
+    public void onViewRecycled(ViewHolder holder) {
+        //Unregister from listeners
+        //holder.itemView.setOnTouchListener(null);
+        super.onViewRecycled(holder);
+    }
+
     @Override
     public void onItemMove(int fromPosition, int toPosition) {
-
+        Collections.swap(songToEdit,fromPosition,toPosition);
+        notifyItemMoved(fromPosition,toPosition);
     }
 
     @Override
     public void onItemSwiped(int position) {
-
+        //Maybe some action with swipe
+        notifyItemChanged(position);
     }
+
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -37,13 +71,21 @@ public class TimeSlicesAdapter extends RecyclerView.Adapter<TimeSlicesAdapter.Vi
 
     @Override
     public int getItemCount() {
-        return 0;
+        return songToEdit.size();
+    }
+
+    public Song getSongToEdit() {
+        return songToEdit;
+    }
+
+    public void setSongToEdit(Song songToEdit) {
+        this.songToEdit = songToEdit;
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder implements
             ItemTouchHelperViewHolder {
 
-        public ViewHolder(View itemView, TextView playerName, TextView playerPriority , ImageView handlerView) {
+        public ViewHolder(View itemView) {
             super(itemView);
         }
 
