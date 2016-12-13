3c35ace794c3216cfff98dc1df64bfcbf21520ac
GiulioPettenuzzo <giulio.pettenuzzo@studenti.unipd.it>
2016-12-13 17:15:01 +0100

    creato il layout dell'item playlist_viewer_item_layout.xml

--- MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/PlaylistView.java
+++ MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/PlaylistView.java
@@ -1,15 +1,29 @@
 package a2016.soft.ing.unipd.metronomepro;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-
-/**
- * Created by giuli on 13/12/2016.
- */
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class PlaylistView extends AppCompatActivity {
-    public void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playlist_view);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
+
 }
--- MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/adapters/SelectPlaylistAdapter.java
+++ MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/adapters/SelectPlaylistAdapter.java
@@ -2,11 +2,16 @@ package a2016.soft.ing.unipd.metronomepro.adapters;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import a2016.soft.ing.unipd.metronomepro.R;
 import a2016.soft.ing.unipd.metronomepro.entities.ParcelablePlaylist;
+import a2016.soft.ing.unipd.metronomepro.entities.Playlist;
 
 /**
  * Created by giuli on 13/12/2016.
@@ -15,7 +20,7 @@ import a2016.soft.ing.unipd.metronomepro.entities.ParcelablePlaylist;
 public class SelectPlaylistAdapter extends RecyclerView.Adapter {
 
     private Context context;
-    private ArrayList<ParcelablePlaylist> arrayPlaylist;
+    private ArrayList<Playlist> arrayPlaylist;
     private int selectPlaylist;
 
     public SelectPlaylistAdapter(Context context) {
@@ -25,12 +30,21 @@ public class SelectPlaylistAdapter extends RecyclerView.Adapter {
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.playlist_viewer, parent, false);
+        RecyclerView.ViewHolder vh = new RecyclerView.ViewHolder(v,(TextView)v.findViewById(R.id.recycler_view_playlist_viewer)) {
+            @Override
+            public String toString() {
+                return super.toString();
+            }
+        };
+
+        return vh;
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+        Playlist s = arrayPlaylist.get(position);
+        holder.setText(s.getName());
     }
 
     @Override
