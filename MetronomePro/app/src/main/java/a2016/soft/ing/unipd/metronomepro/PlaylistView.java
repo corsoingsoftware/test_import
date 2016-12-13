9bcd376ef6ea36939d13cd87e5e1992c782b3d67
GiulioPettenuzzo <giulio.pettenuzzo@studenti.unipd.it>
2016-12-13 15:17:05 +0100

    creata la classe PlaylistView "per fare da adapter" e il corrispondente layout playlist_viewer.xml

---
+++ MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/PlaylistView.java
@@ -0,0 +1,12 @@
+package a2016.soft.ing.unipd.metronomepro;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by giuli on 13/12/2016.
+ */
+
+public class PlaylistView extends AppCompatActivity {
+
+
+}
