ff7fd0ab82bc55ec50a9f77605fe5e2491e20ed2
GiulioPettenuzzo <giulio.pettenuzzo@studenti.unipd.it>
2016-12-14 16:06:37 +0100

    tutto è pronto, bisogna fare il test e vedere se è giusto

--- MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/PlaylistView.java
+++ MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/PlaylistView.java
@@ -4,10 +4,18 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import a2016.soft.ing.unipd.metronomepro.adapters.SelectPlaylistAdapter;
+
 public class PlaylistView extends AppCompatActivity {
+
+    private RecyclerView rVPlaylistItem;
+    private RecyclerView.LayoutManager rVLayoutManager;
+    private SelectPlaylistAdapter playListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +23,13 @@ public class PlaylistView extends AppCompatActivity {
         setContentView(R.layout.activity_playlist_view);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        rVPlaylistItem = (RecyclerView)findViewById(R.id.recicle_view_playlist);
+        rVPlaylistItem.setHasFixedSize(true);
+        rVLayoutManager =  new LinearLayoutManager(this);
+        rVPlaylistItem.setLayoutManager(rVLayoutManager);
+        playListAdapter=new SelectPlaylistAdapter(this);
+        rVPlaylistItem.setAdapter(playListAdapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
