7af6459d0deaa8420e696c50e31dff567b227563
GiulioPettenuzzo <giulio.pettenuzzo@studenti.unipd.it>
2016-12-16 15:58:51 +0100

    FUNZIONA MA IL CODICE VA PULITO

--- MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/PlaylistView.java
+++ MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/PlaylistView.java
@@ -1,6 +1,7 @@
 package a2016.soft.ing.unipd.metronomepro;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,7 +10,16 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 import a2016.soft.ing.unipd.metronomepro.adapters.SelectPlaylistAdapter;
+import a2016.soft.ing.unipd.metronomepro.entities.EntitiesBuilder;
+import a2016.soft.ing.unipd.metronomepro.entities.ParcelablePlaylist;
+import a2016.soft.ing.unipd.metronomepro.entities.Playlist;
+import a2016.soft.ing.unipd.metronomepro.entities.Song;
 
 public class PlaylistView extends AppCompatActivity {
 
@@ -24,12 +34,22 @@ public class PlaylistView extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //per test
+        Playlist p1 = EntitiesBuilder.getPlaylist("prova di playlist 1");
+        Playlist p2 = EntitiesBuilder.getPlaylist("prova di playlist 2");
+        Playlist p3 = EntitiesBuilder.getPlaylist("prova di playlist 3");
+
         rVPlaylistItem = (RecyclerView)findViewById(R.id.recicle_view_playlist);
         rVPlaylistItem.setHasFixedSize(true);
         rVLayoutManager =  new LinearLayoutManager(this);
         rVPlaylistItem.setLayoutManager(rVLayoutManager);
-        playListAdapter=new SelectPlaylistAdapter(this);
+        //per test
+        playListAdapter= new SelectPlaylistAdapter(this,p1);
+        playListAdapter.addPlaylist(p2);
+        playListAdapter.addPlaylist(p3);
+        //speriamo bene
         rVPlaylistItem.setAdapter(playListAdapter);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -39,6 +59,16 @@ public class PlaylistView extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+    private Playlist createTestPlaylist(){
+
+
+        Playlist p = EntitiesBuilder.getPlaylist("prova di playlist");
+
+
+        return p;
+
+
     }
 
 }
--- MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/adapters/SelectPlaylistAdapter.java
+++ MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/adapters/SelectPlaylistAdapter.java
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 
 import a2016.soft.ing.unipd.metronomepro.R;
 import a2016.soft.ing.unipd.metronomepro.adapters.touch.helpers.ItemTouchHelperAdapter;
@@ -26,12 +28,54 @@ public class SelectPlaylistAdapter extends RecyclerView.Adapter<SelectPlaylistAd
     private int selectPlaylist;
     private Playlist PlaylistToEdit;
 
-    public SelectPlaylistAdapter(Context context) {
+    public SelectPlaylistAdapter(Context context, Playlist playlist) {
         this.context = context;
+        PlaylistToEdit = playlist;
+        arrayPlaylist = new ArrayList<>();
+        selectPlaylist = 0;
+        arrayPlaylist.add(selectPlaylist,playlist);
+        selectPlaylist++;
+
 
     }
+    @Override
+    public void onViewRecycled(ViewHolder holder) {
+        super.onViewRecycled(holder);
+    }
+    public void addPlaylist(Playlist playlist){
+        arrayPlaylist.add(selectPlaylist,playlist);
+        selectPlaylist++;
+    }
 
-
+    /**
+     * private void onTimeSliceSelected(TimeSlice ts, int position) {
+     Iterator<OnTimeSliceSelectedListener> iterator= onTimeSliceSelectedListeners.iterator();
+     while(iterator.hasNext()) {
+     OnTimeSliceSelectedListener current=iterator.next();
+     if(current!=null){
+     try{
+     current.onTimeSliceSelected(ts,position);
+     }catch (Exception ex){
+     //unhandledException from listen
+     }
+     }else{
+     iterator.remove();
+     }
+     }
+     }
+     *
+     */
+   /** private void onPlaylistSelected(Playlist pl, int position){
+        Iterator<Playlist> iterator = arrayPlaylist.iterator();
+        while(iterator.hasNext()){
+            Playlist current = iterator.next();
+            if(current!=null){
+                try{
+                    current.
+                }
+            }
+        }
+    }*/
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         //creo una View..
@@ -44,25 +88,27 @@ public class SelectPlaylistAdapter extends RecyclerView.Adapter<SelectPlaylistAd
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
 
-        PlaylistToEdit = arrayPlaylist.get(position);
-        String name = PlaylistToEdit.getName();
-        holder.nameOfPlaylist.setText(name);
+        Playlist p = arrayPlaylist.get(position);
+        holder.nameOfPlaylist.setText(p.getName());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return arrayPlaylist.size();
+
+
     }
 
     @Override
     public void onItemMove(int fromPosition, int toPosition) {
-
+        Collections.swap(PlaylistToEdit, fromPosition, toPosition);
+        notifyItemMoved(fromPosition, toPosition);
     }
 
     @Override
     public void onItemSwiped(int position) {
-
+        notifyItemChanged(position);
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder implements
