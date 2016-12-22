5279da0a8d7ba871251a020aba4e7a115f0807af
FedericoFavotto <federico.favotto94@gmail.com>
2016-12-22 12:06:53 +0100

    commit after merge

--- MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/entities/Song.java
+++ MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/entities/Song.java
@@ -2,13 +2,47 @@ package a2016.soft.ing.unipd.metronomepro.entities;
 
 
 
+import android.os.Parcelable;
+
 import java.util.List;
+
+import a2016.soft.ing.unipd.metronomepro.sound.management.SongPlayer;
+import a2016.soft.ing.unipd.metronomepro.sound.management.SongPlayerManager;
 
 /**
  * Created by feder on 07/12/2016.
  */
 
-public interface Song extends List<TimeSlice>, Streamable {
+public interface Song extends Parcelable {
 
+    /**
+     * returns the name of song
+     * @return songname
+     */
     String getName();
+
+    /**
+     * Ridenominates song
+     * @param newName the new name of song
+     */
+    void setName(String newName);
+
+    /**
+     * returns the id of songs
+     * @return the id of the song
+     */
+    int getId();
+
+    /**
+     * sets the new id
+     * @param newId the new id
+     */
+    void setId(int newId);
+
+    /**
+     * return the manager for the song
+     * @param manager manager that contains the songplayers
+     * @return return the songplayer for this song
+     */
+    SongPlayer getSongPlayer(SongPlayerManager manager);
 }
