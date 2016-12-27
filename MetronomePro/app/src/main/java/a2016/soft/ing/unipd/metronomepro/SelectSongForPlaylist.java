e6ca73bdd3785778651087e7e14e1e0191b446a2
GiulioPettenuzzo <giulio.pettenuzzo@studenti.unipd.it>
2016-12-27 21:04:03 +0100

    importata classe SelectSongForPlaylist

--- MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/SelectSongForPlaylist.java
+++ MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/SelectSongForPlaylist.java
@@ -4,26 +4,83 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.util.ArrayList;
+
+import a2016.soft.ing.unipd.metronomepro.adapters.SelectSongForPlaylistAdapter;
+import a2016.soft.ing.unipd.metronomepro.entities.EntitiesBuilder;
+import a2016.soft.ing.unipd.metronomepro.entities.ParcelableSong;
+import a2016.soft.ing.unipd.metronomepro.entities.Song;
+
 public class SelectSongForPlaylist extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select_song_for_playlist2);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    public class SelectSongForPlaylist extends AppCompatActivity {
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        private RecyclerView rVSelectSong;
+        private RecyclerView.LayoutManager rVLayoutManager;
+        private SelectSongForPlaylistAdapter selectSongForPlaylistAdapter;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+
+            rVSelectSong = (RecyclerView)findViewById(R.id.recicle_song_for_playlist);
+            rVSelectSong.setHasFixedSize(true);
+            rVLayoutManager = new LinearLayoutManager(this);
+            rVSelectSong.setLayoutManager(rVLayoutManager);
+            selectSongForPlaylistAdapter = new SelectSongForPlaylistAdapter(this,provaDiTest());
+            rVSelectSong.setAdapter(selectSongForPlaylistAdapter);
+
+            //potrebbe essere la strada per permettere il click, per il momento non è quella giusta!
+            /**
+             rVSelectSong.setOnClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Toast.makeText(getApplicationContext(),"hai selezionato un pulsante",Toast.LENGTH_SHORT).show();
             }
-        });
+            });
+             */
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_select_song_for_playlist);
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+
+            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            });
+        }
+
+        protected void onSaveInstanceState(Bundle outState) {
+            //prende le canzoni totali e quelle selezionate
+            outState.putParcelableArrayList("song for select", (ArrayList<ParcelableSong>) selectSongForPlaylistAdapter.getArraySongs());
+            outState.putParcelableArrayList("song for select", (ArrayList<ParcelableSong>)selectSongForPlaylistAdapter.getSelectedSongs());
+            super.onSaveInstanceState(outState);
+        }
+
+        public ArrayList<ParcelableSong> provaDiTest(){
+            ArrayList<ParcelableSong> array = new ArrayList<>();
+
+            Song s1 = EntitiesBuilder.getSong("prova di canzone 1");
+            Song s2 = EntitiesBuilder.getSong("prova di canzone 2");
+            Song s3 = EntitiesBuilder.getSong("prova di canzone 3");
+
+            array.add((ParcelableSong) s1);
+            array.add((ParcelableSong) s2);
+            array.add((ParcelableSong) s3);
+
+            return array;
+
+        }
+
     }
+
 
 }
