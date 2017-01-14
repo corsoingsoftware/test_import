2a8eed052658dab88b3044198c76f13810b49f4b
AlbertoMoretto <alberto.moretto.3@studenti.unipd.it>
2017-01-14 16:43:25 +0100

    Implemented MidiSongPlayer interface

--- MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/sound/management/MidiSongPlayer.java
+++ MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/sound/management/MidiSongPlayer.java
@@ -3,26 +3,31 @@ package a2016.soft.ing.unipd.metronomepro.sound.management;
 import android.content.Context;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
 
 import a2016.soft.ing.unipd.metronomepro.entities.MidiSong;
 import a2016.soft.ing.unipd.metronomepro.entities.Song;
 
 /**
- * Created by Alberto on 14/01/17.
+ * Created by Munerato, Alberto on 14/01/17.
  */
 
 public class MidiSongPlayer implements SongPlayer, MediaPlayer.OnCompletionListener {
 
+    public final String TAG = "MidiSongPlayer";
+    private final int UNPOINTED = -1;
     private MediaPlayer player;
     private int playerState;
-    private final int ON_PLAY = 0, ON_PAUSE = 1, ON_STOP = 3;
+    private final int ON_PLAY = 0, ON_PAUSE = 1, ON_STOP = 2;
     private SongPlayerCallback callback;
     private MidiSong[] playlist;
     private int currentSong;
 
     MidiSongPlayer(Context c){
         playerState = ON_STOP;
-
+        currentSong = UNPOINTED;
         player.create(c, null);
         player.setOnCompletionListener(this);
     }
@@ -45,7 +50,7 @@ public class MidiSongPlayer implements SongPlayer, MediaPlayer.OnCompletionListe
 
     @Override
     public void stop() {
-        if(playerState == ON_PLAY) {
+        if(playerState != ON_STOP) {
             playerState = ON_STOP;
             player.stop();
         }
@@ -69,24 +74,55 @@ public class MidiSongPlayer implements SongPlayer, MediaPlayer.OnCompletionListe
     @Override
     public PlayState getState() {
         PlayState ps;
-        if(playerState == ON_PLAY){
-            ps = PlayState.PLAYSTATE_PLAYING;
-        }else if(playerState == ON_PAUSE){
-
+        switch(playerState) {
+            case ON_PLAY :  ps = PlayState.PLAYSTATE_PLAYING;
+            case ON_PAUSE : ps = PlayState.PLAYSTATE_PAUSE;
+            case ON_STOP : ps = PlayState.PLAYSTATE_STOP;
+            default: ps = null;
         }
-        return null;
+        return ps;
     }
 
 
     @Override
     public void write(Song[] songs) {
         currentSong = 0;
-        //for(song:songs) playlist[song] = (MidiSong) songs[i++];
-        //player.setDataSource(playlist[currentSong].getPath());
+        playlist = new MidiSong[songs.length];
+        for(int i = 0; i < songs.length; i++) playlist[i] = (MidiSong) songs[i];
+        try {
+            player.setDataSource(playlist[currentSong].getPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.d(TAG,"Midi not found or invalid path");
+        }
+        try {
+            player.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void onCompletion(MediaPlayer mp) {
-        callback.playEnded();
+        currentSong++;
+        if(currentSong < playlist.length){
+            try {
+                player.setDataSource(playlist[currentSong].getPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.d(TAG,"Midi not found or invalid path");
+            }
+            try {
+                player.prepare();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            player.start();
+        }
+        else{
+            currentSong = UNPOINTED;
+            playlist = null;
+            callback.playEnded(this);
+        }
     }
 }
