76935bdaff58767b4fffd503b6bb2076382b78b2
OmarVesco <omar.vesco@studenti.unipd.it>
2017-01-14 15:49:35 +0100

    Commit to get Moretto's class

--- MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/sound/management/MultipleSongPlayerManager.java
+++ MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/sound/management/MultipleSongPlayerManager.java
@@ -16,6 +16,7 @@ public class MultipleSongPlayerManager implements SongPlayerManager {
 
     private final static int PLAYERS=2;
     private AudioTrackSongPlayer audioTrackSongPlayer;
+
     private int timeSlicesPlayerState;
     private int midiPlayerState;
     private LinkedBlockingQueue<Song> songQueue;
@@ -46,26 +47,33 @@ public class MultipleSongPlayerManager implements SongPlayerManager {
         entrySong.getSongPlayer(this).load(entrySong);
     }
 
-    public void write(Song[] songs) {
+    /*public void write(Song[] songs, Song[] ) {
 
         //entrySong.getSongPlayer(this).write(entrySong);
 
         int i = 0;
-        int typeChanged=0;
-        Class s=songs[i].getClass();
-        while(i < songs.length && typeChanged<PLAYERS) {
-            Song currSong = songs[i++];
-            if(currSong.getClass()!=s){
-                typeChanged++;
-            }
-            while(songs[i] instanceof currSong. && i < songs.length) {
+        int typeChanged = 0;
+        Class s = songs[i].getClass();
+        Song currSong = songs[i++];
 
-                songQueue.add(songs[i]);
-                i++;
+        while(i < songs.length && typeChanged < PLAYERS) {
+
+            if(currSong.getClass() != s){
+                typeChanged++;
+                currSong = songs[i];
+                s = songs[i].getClass();
             }
+            else {
+                songQueue.add(songs[i]);
+            }
+
+            i++;
         }
 
-    }
+
+    }*/
+
+
 
     public byte[] getSong(Song entrySong) {
         return entrySong.getSongPlayer(this).getSong(entrySong);
