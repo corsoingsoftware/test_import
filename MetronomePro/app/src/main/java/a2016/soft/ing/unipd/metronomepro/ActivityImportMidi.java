f4db520c2c32aec7ce381ab01987c8be82cfa089
AlbertoMoretto <alberto.moretto.3@studenti.unipd.it>
2017-01-27 16:02:12 +0100

    Add import of midi file into internal directory

--- MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/ActivityImportMidi.java
+++ MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/ActivityImportMidi.java
@@ -3,6 +3,7 @@ package a2016.soft.ing.unipd.metronomepro;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -15,7 +16,16 @@ import android.widget.Toast;
 import com.nbsp.materialfilepicker.MaterialFilePicker;
 import com.nbsp.materialfilepicker.ui.FilePickerActivity;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.channels.FileChannel;
 import java.util.regex.Pattern;
+
+import a2016.soft.ing.unipd.metronomepro.data.access.layer.DataProvider;
+import a2016.soft.ing.unipd.metronomepro.data.access.layer.DataProviderBuilder;
+import a2016.soft.ing.unipd.metronomepro.entities.EntitiesBuilder;
+import a2016.soft.ing.unipd.metronomepro.entities.MidiSong;
 
 import static android.R.attr.data;
 
@@ -27,11 +37,22 @@ public class ActivityImportMidi extends AppCompatActivity {
 
     public static final int PERMISSIONS_REQUEST_CODE = 0;
     public static final int FILE_PICKER_REQUEST_CODE = 1;
+    private DataProvider db;
+    private File midiStorageDir = new File(Environment.getExternalStorageDirectory()+"/midiStorageDir");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_import_midi);
+
+        db = DataProviderBuilder.getDefaultDataProvider(this);
+
+
+        if(!midiStorageDir.exists()){
+            midiStorageDir.mkdir();
+        }
 
         Button importButton = (Button) findViewById(R.id.import_midi);
         importButton.setOnClickListener(new View.OnClickListener() {
@@ -69,7 +90,19 @@ public class ActivityImportMidi extends AppCompatActivity {
                 String filePath = data.getStringExtra(FilePickerActivity.RESULT_FILE_PATH);
                 if (filePath != null) {
                     Log.d("Path: ", filePath);
-                    Toast.makeText(this, "Picked file: " + filePath, Toast.LENGTH_LONG).show();
+
+                    String midiTitle = filePath.substring(filePath.lastIndexOf("/")+1);
+                    String midiPath = midiStorageDir.getAbsolutePath() + "/" + midiTitle;
+                    if(copyFile(filePath,midiPath)){
+                        Toast.makeText(this, "File copied in : " + midiPath, Toast.LENGTH_LONG).show();
+                        MidiSong md = EntitiesBuilder.getMidiSong();
+                        md.setName(midiTitle);
+                        md.setPath(midiPath);
+                    }
+                    else{
+                        Toast.makeText(this, "Import failed", Toast.LENGTH_LONG).show();
+                    }
+
                 }
             }
         }
@@ -84,4 +117,28 @@ public class ActivityImportMidi extends AppCompatActivity {
                 .withTitle("Sample title")
                 .start();
     }
+
+    private static boolean copyFile(String from, String to) {
+        try {
+            File sd = Environment.getExternalStorageDirectory();
+            if (sd.canWrite()) {
+                int end = from.toString().lastIndexOf("/");
+                String str1 = from.toString().substring(0, end);
+                String str2 = from.toString().substring(end+1, from.length());
+                File source = new File(str1, str2);
+                File destination= new File(to, str2);
+                if (source.exists()) {
+                    FileChannel src = new FileInputStream(source).getChannel();
+                    FileChannel dst = new FileOutputStream(destination).getChannel();
+                    dst.transferFrom(src, 0, src.size());
+                    src.close();
+                    dst.close();
+                }
+            }
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
 }
