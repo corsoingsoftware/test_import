99fd2b12224e6f1efec593b1b93b65c9a12f7e49
GiulioPettenuzzo <giulio.pettenuzzo@studenti.unipd.it>
2017-02-06 23:44:13 +0100

    adattato il codice alle code review

--- MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/adapters/SelectSongForPlaylistAdapter.java
+++ MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/adapters/SelectSongForPlaylistAdapter.java
@@ -23,11 +23,11 @@ import a2016.soft.ing.unipd.metronomepro.data.access.layer.DataProviderBuilder;
 import a2016.soft.ing.unipd.metronomepro.entities.Song;
 
 /**
- * Created by giuli on 27/12/2016.
+ * Created by giuli on 27/11/2016.
  */
 
 /**
- * SelectSongForPlaylist and SelectSongForPlaylist are built for insert the songs that are saved into the database, into a
+ * SelectSongForPlaylistAdapter and SelectSongForPlaylist are built for insert the songs that are saved into the database, into a
  * playlist that is recived from an other activity.
  * This adapter allows to select and deselect the songs that will be included in the playlist
  */
@@ -49,39 +49,57 @@ public class SelectSongForPlaylistAdapter extends RecyclerView.Adapter<a2016.sof
         this.selectedSongs = selectedSongs;
     }
 
+    /**
+     * @return the songs in recycleView
+     */
     public ArrayList<Song> getArraySongs(){
         return arraySongs;
     } //return the list of the all songs
+
+    /**
+     * @return the songs that user selected
+     */
     public ArrayList<Song> getSelectedSongs(){//return the list of the songs Selected
         return selectedSongs;
     }
+
+    /**
+     * @param song: the song to add
+     */
     public void addSong(Song song){
         arraySongs.add(song);
     }
     //this is the creator of the ViewHolder
     //see documentation for more information
     @Override
+    /**
+     * this is the creator of the ViewHolder
+     * see documentation for more information
+     */
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.select_song_for_playlist_item, parent, false);
-        ViewHolder vh = new ViewHolder(v, (TextView)v.findViewById(R.id.item_song_for_playlist));
-        return vh;
+        View viewOfRecycle = LayoutInflater.from(parent.getContext()).inflate(R.layout.select_song_for_playlist_item, parent, false);
+        ViewHolder viewHolder = new ViewHolder(viewOfRecycle, (TextView)viewOfRecycle.findViewById(R.id.item_song_for_playlist));
+        return viewHolder;
 
     }
 
     @Override
+    /**
+     * This method internally calls onBindViewHolder(ViewHolder, int) to update the RecyclerView.ViewHolder
+     * contents with the item at the given position and also sets up some private fields to be used by RecyclerView.
+     * for more information: https://developer.android.com/reference/android/support/v7/widget/RecyclerView.Adapter.html
+     */
     public void onBindViewHolder(ViewHolder holder, final int position) {
         Song song = arraySongs.get(position);
         holder.nameOfSong.setText(song.getName());//to see the name of the songs in the RecycleView
     //below the code for hilight an item when pressed
-        View.OnClickListener a = new View.OnClickListener() {
+        View.OnClickListener itemClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //when the item is deselecting
                 if(v.isSelected()){
                     v.setSelected(false); //set the background golour
-
-                    Song song = arraySongs.get(position); //remove the songe from selected songs
-                    selectedSongs.remove(song);
+                    selectedSongs.remove(arraySongs.get(position));//remove the songe from selected songs
                 }
                 //when the item is selecting
                 else{
@@ -93,7 +111,7 @@ public class SelectSongForPlaylistAdapter extends RecyclerView.Adapter<a2016.sof
             }
         };
         //the listener is activate when the item is building
-        holder.itemView.setOnClickListener(a);
+        holder.itemView.setOnClickListener(itemClickListener);
         //when an item is recycled it take the settings of the first item in the list
         //if the first item is selected, the recycled item will take the background of the first item whitch is wrong
         //without this if-else, when you press an item more than one item will higtlight
@@ -112,15 +130,19 @@ public class SelectSongForPlaylistAdapter extends RecyclerView.Adapter<a2016.sof
     }
 
     @Override
+    /**
+     * this method explain what heppens when the view is recycle
+     */
     public void onViewRecycled(ViewHolder holder) {
         super.onViewRecycled(holder);
         //when you scroll up the list, the listener of the new item is disactivate
         //so they will set in the right settings
         holder.itemView.setOnClickListener(null);
     }
-
-    //A ViewHolder describes an item view and metadata about its place within the RecyclerView.
-    //for more information: https://developer.android.com/reference/android/support/v7/widget/RecyclerView.ViewHolder.html
+    /**
+     *A ViewHolder describes an item view and metadata about its place within the RecyclerView.
+     *for more information: https://developer.android.com/reference/android/support/v7/widget/RecyclerView.ViewHolder.html
+     */
     public class ViewHolder extends RecyclerView.ViewHolder{
 
         TextView nameOfSong;
