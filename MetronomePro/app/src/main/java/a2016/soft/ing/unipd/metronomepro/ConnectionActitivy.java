4ec345da1bd82ccaccf0ddfdcf001930aa928271
FedericoFavotto <federico.favotto94@gmail.com>
2017-02-07 11:48:24 +0100

    Commit per ts

---
+++ MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/ConnectionActitivy.java
@@ -0,0 +1,20 @@
+package a2016.soft.ing.unipd.metronomepro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Questa classe deve permettere all'utente di associarsi al server o cercarne uno.
+ * Una volta fatto avvia l'activity di Omar nel caso sia server, altrimenti la clientactivity
+ */
+public class ConnectionActitivy extends AppCompatActivity {
+    /**
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connection_actitivy);
+    }
+}
