47a662f17f146c23322c4a1ab439e2876c5db412
OmarVesco <omar.vesco@studenti.unipd.it>
2017-02-08 15:32:45 +0100

    Commit final

--- MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/ConnectionActivity.java
+++ MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/ConnectionActivity.java
@@ -2,6 +2,8 @@ package a2016.soft.ing.unipd.metronomepro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import org.group3.sync.Manager;
 import org.group3.sync.ManagerFactory;
@@ -11,15 +13,25 @@ import org.group3.sync.ServerActionListener;
 import org.group3.sync.ServerInfo;
 import org.group3.sync.exception.ErrorCode;
 
+import a2016.soft.ing.unipd.metronomepro.entities.EntitiesBuilder;
+import a2016.soft.ing.unipd.metronomepro.entities.Song;
+import a2016.soft.ing.unipd.metronomepro.entities.TimeSlice;
+import a2016.soft.ing.unipd.metronomepro.entities.TimeSlicesSong;
+import a2016.soft.ing.unipd.metronomepro.sound.management.SongPlayer;
+import a2016.soft.ing.unipd.metronomepro.sound.management.SongPlayerServiceCaller;
+
 import static java.lang.System.out;
 
 /**
  * Questa classe deve permettere all'utente di associarsi al server o cercarne uno.
  * Una volta fatto avvia l'activity di Omar nel caso sia server, altrimenti la clientactivity
  */
-public class ConnectionActivity extends AppCompatActivity implements ServerActionListener {
+public class ConnectionActivity extends AppCompatActivity implements ServerActionListener, SongPlayerServiceCaller.SongPlayerServiceCallerCallback {
 
     Manager connectionManager;
+    SongPlayerServiceCaller spsc;
+    long timeDifference;
+    TimeSlicesSong ts;
     /**
      *
      * @param savedInstanceState
@@ -30,8 +42,23 @@ public class ConnectionActivity extends AppCompatActivity implements ServerActio
         setContentView(R.layout.activity_connection_activity);
         connectionManager= ManagerFactory.bluetoothInstance();//grande Lì!
         try {
-            Server server=connectionManager.newServer(this, this);
-
+            final Server server=connectionManager.newServer(this, this);
+            spsc = new SongPlayerServiceCaller(this, this);
+            Button b = (Button)findViewById(R.id.button2);
+            b.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    final long start = System.currentTimeMillis()+2000;
+                    server.broadcastStart(start);
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            while(System.currentTimeMillis()<start);
+                            spsc.write(new Song[]{ts});
+                        }
+                    }).start();
+                }
+            });
         }
         catch (Exception ex){
 
@@ -41,7 +68,7 @@ public class ConnectionActivity extends AppCompatActivity implements ServerActio
 
     @Override
     public void onClientConnected(Peer peer) {
-        out.println(getClass().getEnclosingMethod().getName());
+        out.println("onClientConnected");
     }
 
     @Override
@@ -67,5 +94,17 @@ public class ConnectionActivity extends AppCompatActivity implements ServerActio
     @Override
     public void onError(ErrorCode error) {
         out.println("onError");
+    }
+
+    @Override
+    public void serviceConnected() {
+        ts= EntitiesBuilder.getTimeSlicesSong();
+        ts.setName("prova");
+        TimeSlice a= new TimeSlice();
+        a.setBpm(60);
+        a.setDurationInBeats(200);
+        ts.add(a);
+        spsc.load(ts);
+
     }
 }
