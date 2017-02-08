8d9fe4c682629a9d5a406c9a351ace7a2c3dfb32
FedericoFavotto <federico.favotto94@gmail.com>
2017-02-08 17:05:36 +0100

    Fixed clientActivity

--- MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/ClientActivity.java
+++ MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/ClientActivity.java
@@ -1,16 +1,199 @@
 package a2016.soft.ing.unipd.metronomepro;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import org.group3.sync.Client;
+import org.group3.sync.ClientActionListener;
+import org.group3.sync.Manager;
+import org.group3.sync.ManagerFactory;
+import org.group3.sync.MetroConfig;
+import org.group3.sync.Peer;
+import org.group3.sync.exception.AdapterNotActivatedException;
+import org.group3.sync.exception.ErrorCode;
+
+import java.util.List;
+
+import a2016.soft.ing.unipd.metronomepro.entities.EntitiesBuilder;
+import a2016.soft.ing.unipd.metronomepro.entities.MidiSong;
+import a2016.soft.ing.unipd.metronomepro.entities.Song;
+import a2016.soft.ing.unipd.metronomepro.entities.TimeSlicesSong;
+import a2016.soft.ing.unipd.metronomepro.sound.management.SongPlayerServiceCaller;
+
+import static java.lang.System.out;
 
 /**
  * Activity che riceve i comandi del server e li esegue
  */
-public class ClientActivity extends AppCompatActivity {
+public class ClientActivity extends AppCompatActivity implements ClientActionListener, SongPlayerServiceCaller.SongPlayerServiceCallerCallback {
 
+    Manager connectionManager;
+    Client client;
+    SongPlayerServiceCaller spsc;
+    boolean bluetoothOk=false, serviceOk=false;
+    long timeDiff;
+    private static final int REQUEST_ENABLE_BT=1;
+    /**
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_client);
+        setContentView(R.layout.activity_connection_activity);
+
+        spsc = new SongPlayerServiceCaller(this,this);
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+        if(permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+
+            } else {
+
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 5);
+            }
+        }
+        try {
+
+            connectionManager= ManagerFactory.bluetoothInstance();
+            client=connectionManager.newClient(this,this);
+
+        }catch (AdapterNotActivatedException ex){
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+        }catch (Exception ex){
+            //def action
+            ex.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case REQUEST_ENABLE_BT:
+                if(resultCode==RESULT_OK){
+                    try {
+                        client = connectionManager.newClient(this, this);
+                    }catch (Exception ex){
+                        ex.printStackTrace();
+                    }
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onPeerListReady(List<Peer> list) {
+        out.println("");
+        if(list.size()>0){
+            client.connectToPeer(list.get(0));
+        }
+
+    }
+
+    @Override
+    public void onConnecting() {
+
+        out.println("");
+    }
+
+    @Override
+    public void onConnected() {
+
+        out.println("");
+    }
+
+    @Override
+    public void onSynchronized(long timeDifference) {
+        timeDiff=timeDifference;
+        bluetoothOk=true;
+        if(bluetoothOk&&serviceOk){
+            faiqualcosa();
+        }
+        out.println("");
+    }
+
+    private void faiqualcosa(){
+
+    }
+
+    @Override
+    public void onDisconnected() {
+
+        out.println("");
+    }
+
+    @Override
+    public void onError(ErrorCode error) {
+
+        out.println("");
+    }
+
+    @Override
+    public void onReceiveSet(MetroConfig config) {
+
+        out.println("");
+    }
+
+    @Override
+    public void onReceiveStart(MetroConfig config, long time, long delay) {
+
+        spsc.write(new Song[]{});
+        long a=System.currentTimeMillis();
+        long b=time+(timeDiff);
+        while(a<b-10){
+            a=System.currentTimeMillis();
+        }
+        spsc.play();
+        a=System.currentTimeMillis();
+        out.println(a-b);
+        out.println("");
+    }
+
+    @Override
+    public void onReceiveStop() {
+
+        out.println("");
+    }
+
+    @Override
+    public void onReceiveGeneralMessage(byte[] message) {
+
+        out.println("");
+    }
+
+
+    @Override
+    public void serviceConnected() {
+//        ts= EntitiesBuilder.getTimeSlicesSong();
+//
+//        ts.setName("prova");
+//        TimeSlice a= new TimeSlice();
+//        a.setBpm(60);
+//        a.setDurationInBeats(3);
+//        ts.add(a);
+//        a= new TimeSlice();
+//        a.setBpm(120);
+//        a.setDurationInBeats(4);
+//        ts.add(a);
+//        a= new TimeSlice();
+//        a.setBpm(300);
+//        a.setDurationInBeats(4);
+//        ts.add(a);
+//        spsc.load(ts);
+        serviceOk=true;
+        if(bluetoothOk&&serviceOk){
+            faiqualcosa();
+        }
     }
 }
+
