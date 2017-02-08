8fb1fc87a79637ada590edb8046f4d39ba78c52a
Francesco Loro <francesco.official@gmail.com>
2017-02-08 17:03:22 +0100

    ShowPeersAdapter.java finito di implementare i metodi per la recycler view

--- MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/adapters/ShowPeersAdapter.java
+++ MetronomePro/app/src/main/java/a2016/soft/ing/unipd/metronomepro/adapters/ShowPeersAdapter.java
@@ -1,19 +1,24 @@
 package a2016.soft.ing.unipd.metronomepro.adapters;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import org.group3.sync.Peer;
 
 import java.util.ArrayList;
 
+import a2016.soft.ing.unipd.metronomepro.R;
 import a2016.soft.ing.unipd.metronomepro.adapters.touch.helpers.ItemTouchHelperAdapter;
+import a2016.soft.ing.unipd.metronomepro.adapters.touch.helpers.ItemTouchHelperViewHolder;
 
 /**
  * Created by Francesco on 08/02/2017.
  */
 
-public class ShowPeersAdapter extends RecyclerView.Adapter<ModifyPlaylistAdapter.ViewHolder> implements ItemTouchHelperAdapter {
+public class ShowPeersAdapter extends RecyclerView.Adapter<ShowPeersAdapter.ViewHolder> implements ItemTouchHelperAdapter {
 
     private ArrayList<Peer> peerList;
 
@@ -26,6 +31,44 @@ public class ShowPeersAdapter extends RecyclerView.Adapter<ModifyPlaylistAdapter
     }
 
     @Override
+    public int getItemCount() {
+        return peerList.size();
+    }
+
+    public void addPeer(Peer peerToAdd) {
+        peerList.add(peerToAdd);
+        notifyItemInserted(peerList.size() - 1);
+    }
+
+    public void addPeer(int index, Peer peerToAdd) {
+        peerList.add(index, peerToAdd);
+        notifyItemInserted(index);
+    }
+
+    public void removePeer(Peer peerToRemove) {
+        peerList.remove(peerToRemove);
+        notifyItemRemoved(peerList.indexOf(peerToRemove));
+    }
+
+    public void removePeer(int indexOfPeerToRemove) {
+        peerList.remove(indexOfPeerToRemove);
+        notifyItemRemoved(indexOfPeerToRemove);
+    }
+
+    public Peer getPeer(int index) {
+        return peerList.get(index);
+    }
+
+    public ArrayList getList() {
+        return peerList;
+    }
+
+    public int getIndexOf(Peer peer) {
+        return peerList.indexOf(peer);
+    }
+
+
+    @Override
     public void onItemMove(int fromPosition, int toPosition) {
 
     }
@@ -36,50 +79,38 @@ public class ShowPeersAdapter extends RecyclerView.Adapter<ModifyPlaylistAdapter
     }
 
     @Override
-    public ModifyPlaylistAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public ShowPeersAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.show_peers_activity_layout, parent, false);
+        ViewHolder vh = new ViewHolder(v, (TextView) v.findViewById(R.id.peer_item), v.findViewById((R.id.handle)));
+        return vh;
     }
 
     @Override
-    public void onBindViewHolder(ModifyPlaylistAdapter.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Peer p = peerList.get(position);
+        holder.peerName.setText(p.getName());
     }
 
-    @Override
-    public int getItemCount() {
-        return peerList.size();
-    }
 
-    public void addPeer(Peer peerToAdd){
-        peerList.add(peerToAdd);
-        notifyItemInserted(peerList.size()-1);
-    }
+    static class ViewHolder extends RecyclerView.ViewHolder implements ItemTouchHelperViewHolder {
 
-    public void addPeer(int index, Peer peerToAdd){
-        peerList.add(index, peerToAdd);
-        notifyItemInserted(index);
-    }
+        TextView peerName;
 
-    public void removePeer(Peer peerToRemove){
-        peerList.remove(peerToRemove);
-        notifyItemRemoved(peerList.indexOf(peerToRemove));
-    }
+        public ViewHolder(View itemView, TextView peerName) {
+            super(itemView);
+            this.peerName = peerName;
+        }
 
-    public  void removePeer(int indexOfPeerToRemove){
-        peerList.remove(indexOfPeerToRemove);
-        notifyItemRemoved(indexOfPeerToRemove);
-    }
+        @Override
+        public void onItemSelected() {
 
-    public Peer getPeer(int index){
-        return peerList.get(index);
-    }
+        }
 
-    public ArrayList getList(){
-        return peerList;
-    }
+        @Override
+        public void onItemClear() {
 
-    public int getIndexOf(Peer peer){
-        return peerList.indexOf(peer);
+        }
     }
-
 }
+
+
